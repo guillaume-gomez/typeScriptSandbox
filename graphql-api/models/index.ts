06e8ba0ad3e1c6358771cf320f7adda72d118326
Guillaume Gomez <guillaume.gomez846@gmail.com>
2019-02-22 10:15:52 +0100

    update db connection

--- graphql-api/models/index.ts
+++ graphql-api/models/index.ts
@@ -5,8 +5,34 @@ const sequelize = new Sequelize(
   process.env.DATABASE_USER,
   process.env.DATABASE_PASSWORD,
   {
+    host: process.env.DATABASE_HOST,
     dialect: 'postgres',
+    port: process.env.DATABASE_PORT,
+    pool: {
+        max: 20,
+        min: 0,
+        idle: 20000,
+        handleDisconnects: true
+    },
+    dialectOptions: {
+        requestTimeout:100000
+    },
+    define: {
+        freezeTableName: true
+    }
   },
 );
 
+const models = {
+  User: sequelize.import('./user'),
+  Message: sequelize.import('./message'),
+};
+
+Object.keys(models).forEach(key => {
+  if ('associate' in models[key]) {
+    models[key].associate(models);
+  }
+});
+
 export { sequelize };
+export default models;
