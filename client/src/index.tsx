9224f2198b83b8910ae47eaf82cf346289975a8b
guillaume-gomez <guillaume.gomez846@gmail.com>
2019-02-24 12:55:22 +0100

    use dotenv

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,13 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
+import ApolloClient from "apollo-boost";
+
+const client = new ApolloClient({
+  uri: process.env.SERVER_URL
+});
+
+
 import { Hello } from "./components/Hello";
 
 ReactDOM.render(
